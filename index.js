324750a5c4781e7b3380375cef0384aadec857f4
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-11-22 19:23:11 +0100

    Documents function

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const defaultLang = 'no_nb'
+const defaultLanguage = 'no_nb'
 
-function getDocumentDefinition (id, lang = defaultLang) {
-  const definition = `./definitions/${id}-${lang}`
-  const fallback = `./definitions/${id}-${defaultLang}`
+/**
+ * Returns function for selected document definition
+ * @param {string} id The id for document definition 
+ * @param {string} language The language for the definition
+ * @returns {function} 
+ */
+function getDocumentDefinition (id, language = defaultLanguage) {
+  const definition = `./definitions/${id}-${language}`
+  const fallback = `./definitions/${id}-${defaultLanguage}`
   try {
     return require(definition)
   } catch (error) {
