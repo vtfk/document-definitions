a0a9362dd151da0560a6415d22420afedc93d07d
Mats Andreassen <matsand@live.no>
2020-12-02 11:26:50 +0100

    Updated tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,16 @@
 const test = require('ava')
 const getDefinitions = require('../index')
+const isFunction = (dd) => typeof dd === 'function'
+const isObject = (dd) => typeof dd === 'object'
 
 test('returns default languange if no language is supplied', t => {
-  const dd = getDefinitions('brevmal')
-  const isFunction = typeof dd === 'function'
-  t.is(isFunction, true)
+  const [dd, styles] = getDefinitions('brevmal')
+  t.is(isFunction(dd), true)
+  t.is(isObject(styles), true)
 })
 
 test('returns default languange if none existing language is supplied', t => {
-  const dd = getDefinitions('brevmal', 'blahblahbla')
-  const isFunction = typeof dd === 'function'
-  t.is(isFunction, true)
+  const [dd, styles] = getDefinitions('brevmal', 'blahblahbla')
+  t.is(isFunction(dd), true)
+  t.is(isObject(styles), true)
 })
