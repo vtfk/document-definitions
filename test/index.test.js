768eb3e1b8362c20a2735a98481b19e74999daac
Mats Andreassen <matsand@live.no>
2021-01-08 14:06:37 +0100

    Oppdaterte tester

--- test/index.test.js
+++ test/index.test.js
@@ -3,14 +3,26 @@ const getDefinitions = require('../index')
 const isFunction = (dd) => typeof dd === 'function'
 const isObject = (dd) => typeof dd === 'object'
 
-test('returns default languange if no language is supplied', t => {
+test('returns default languange if no language is supplied for brevmal', t => {
   const [dd, styles] = getDefinitions('brevmal')
   t.is(isFunction(dd), true)
   t.is(isObject(styles), true)
 })
 
-test('returns default languange if none existing language is supplied', t => {
+test('returns default languange if none existing language is supplied for brevmal', t => {
   const [dd, styles] = getDefinitions('brevmal', 'blahblahbla')
+  t.is(isFunction(dd), true)
+  t.is(isObject(styles), true)
+})
+
+test('returns default languange if no language is supplied for notatmal', t => {
+  const [dd, styles] = getDefinitions('notatmal')
+  t.is(isFunction(dd), true)
+  t.is(isObject(styles), true)
+})
+
+test('returns default languange if none existing language is supplied for notatmal', t => {
+  const [dd, styles] = getDefinitions('notatmal', 'blahblahbla')
   t.is(isFunction(dd), true)
   t.is(isObject(styles), true)
 })
